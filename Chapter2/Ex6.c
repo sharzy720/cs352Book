a431134fa2299c5fb6196e4070e944a334661d02
Johnathyn Strong <johnathynstrong@gmail.com>
2022-02-19 21:05:42 -0500

    Finished chapter 2 example 6

--- Chapter2/Ex6.c
+++ Chapter2/Ex6.c
@@ -1,10 +1,10 @@
-//Write a program to compute the probability that we can toss some number of heads, n, in a row using a coin.
-//Compute the probability by simulating the coin toss with a random number.
-//A Java solution to this problem is presented in Section 4.7 of JBD.
-//The function rand() mentioned in the previous exercise returns a random integer
-//  in the range 0 to RAND_MAX,which is defined in stdlib.h.
-//You can simulate a coin toss by treating a return value from rand() that is less
-//  than RAND_MAX/2 as a head and treating any other value as a tail.
+// Write a program to compute the probability that we can toss some number of heads, n, in a row using a coin.
+// Compute the probability by simulating the coin toss with a random number.
+// A Java solution to this problem is presented in Section 4.7 of JBD.
+// The function rand() mentioned in the previous exercise returns a random integer
+//   in the range 0 to RAND_MAX,which is defined in stdlib.h.
+// You can simulate a coin toss by treating a return value from rand() that is less
+//   than RAND_MAX/2 as a head and treating any other value as a tail.
 
 #include <stdio.h>
 #include <stdlib.h>
